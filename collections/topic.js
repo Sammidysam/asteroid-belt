e1b1e9b5530254065aef61c86ed433d489fab694
Sammidysam <sammidysam@gmail.com>
2014-10-18 18:20:42 -0400

    Add explanation of topics

--- collections/topic.js
+++ collections/topic.js
@@ -1,1 +1,17 @@
 Topics = new Meteor.Collection("topics");
+
+/*
+ * Topics will have:
+ *   name: a string of the name of this topic
+ *     this name will be seen on the list of topics
+ *   description: a string of the description of this topic
+ *     this will be shown when the given topic is selected to be voted on
+ *   options: an array of all of the options of this topic
+ *     each option has
+ *       id: its id
+ *       name: its name
+ *
+ * Example:
+ *   name: "ACSL President '15--'16"
+ *   description: "Who should be the ACSL President for the school year of 2015--2016?"
+ */
