f273068d90dc40e3158c8be800ebe6b07724c7ed
Sammidysam <sammidysam@gmail.com>
2014-10-22 21:37:27 -0400

    Let's save user voting functionality for later

--- collections/vote.js
+++ collections/vote.js
@@ -3,12 +3,10 @@ Votes = new Meteor.Collection("votes");
 /*
  * Votes will have:
  *   topic_id: the id of the topic that this vote was for
- *   user_id: the id of the user that this vote was for
  *   option_id: the id of the option that this vote was for
  *     the correct option will be identified because the vote has a topic
  *
  * Example:
  *   topic_id: some_id
- *   user_id: some_id
  *   option_id: some_id
  */
