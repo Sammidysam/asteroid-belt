4ce809531a72e1d8b1691057ded770a32a2d939f
Sammidysam <sammidysam@gmail.com>
2015-01-18 12:46:12 -0500

    Options can now be voted for

--- client/topics/show.js
+++ client/topics/show.js
@@ -1,3 +1,17 @@
+Template.topicShowOption.events({
+	"click": function (event) {
+		var topicId = window.location.href.split("/").slice(-1)[0];
+		
+		Votes.insert({
+			topic_id: topicId,
+			option_id: this._id
+		}, function (err, id) {
+			if (err)
+				alert(err);
+		});
+	}
+});
+
 Template.topicShowOption.helpers({
 	votes: function () {
 		/* Have to get parent ID from URL since context does not provide it. */
