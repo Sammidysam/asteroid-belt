0b7840b9f6ebc945f712ab2b8551db0b6fa3f22b
Sammidysam <sammidysam@gmail.com>
2015-01-25 19:48:20 -0500

    Add documentation for user functionality

--- collections/vote.js
+++ collections/vote.js
@@ -5,6 +5,7 @@ Votes = new Meteor.Collection("votes");
  *   topic_id: the id of the topic that this vote was for
  *   option_id: the id of the option that this vote was for
  *     the correct option will be identified because the vote has a topic
+ *   creator: the email of the user who created this vote
  *
  * Example:
  *   topic_id: some_id
